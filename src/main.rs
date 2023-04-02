f5447c382ef3e68d48d855928c7125198376b898
d0bhareach <viacheslav.modestov@gmail.com>
2023-04-02 20:20:26 +0200

    use ratatui and termion, write about project in readme

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 use chrono::prelude::*;
-use crossterm::{
-    event::{self, Event as CEvent, KeyCode},
-    terminal::{disable_raw_mode, enable_raw_mode},
+use termion::{
+    screen::IntoAlternateScreen,
+    event::{Event, Key},
+    input::TermRead,
+    raw::IntoRawMode,
+    // raw::RawTerminal,
 };
 use rand::{distributions::Alphanumeric, prelude::*};
 use serde::{Deserialize, Serialize};
-use std::fs;
-use std::io;
-use std::sync::mpsc;
-use std::thread;
-use std::time::{Duration, Instant};
+use std::{fs, cmp::Ordering, thread,
+    io::{self, stdout, stdin},
+    sync::mpsc,
+    time::{Duration, Instant}
+};
 use thiserror::Error;
 use ratatui::{
-    backend::CrosstermBackend,
+    backend::TermionBackend, Terminal,
     layout::{Alignment, Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
     widgets::{
         Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs,
     },
-    Terminal,
 };
 
 const DB_PATH: &str = "./data/db.json";
@@ -30,11 +32,6 @@ pub enum Error {
     ReadDBError(#[from] io::Error),
     #[error("error parsing the DB file: {0}")]
     ParseDBError(#[from] serde_json::Error),
-}
-
-enum Event<I> {
-    Input(I),
-    Tick,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -62,7 +59,14 @@ impl From<MenuItem> for usize {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    enable_raw_mode().expect("can run in raw mode");
+    let stdout = stdout();
+    let stdout = stdout.into_alternate_screen().unwrap();
+    let stdout = stdout.into_raw_mode().unwrap();
+    let mut stdin = stdin().events();
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
 
     let (tx, rx) = mpsc::channel();
     let tick_rate = Duration::from_millis(200);
@@ -73,24 +77,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .checked_sub(last_tick.elapsed())
                 .unwrap_or_else(|| Duration::from_secs(0));
 
-            if event::poll(timeout).expect("poll works") {
-                if let CEvent::Key(key) = event::read().expect("can read events") {
-                    tx.send(Event::Input(key)).expect("can send events");
-                }
-            }
-
-            if last_tick.elapsed() >= tick_rate {
-                if let Ok(_) = tx.send(Event::Tick) {
-                    last_tick = Instant::now();
-                }
+            let d = Duration::from_millis(0);
+            let comparison = timeout.cmp(&d);
+            
+            if Ordering::is_le(comparison) {
+                    if let Some(evt) = stdin.next() {
+                        match evt {
+                            Ok(e) => tx.send(e).unwrap(),
+                            _ => continue,
+                        }
+                    }
+                last_tick = Instant::now();    
             }
         }
     });
 
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
 
     let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
     let mut active_menu_item = MenuItem::Home;
@@ -100,13 +101,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         terminal.draw(|rect| {
             let size = rect.size();
+            // Direction::Vertical - screen is about to be splitted on chuncs vertically
+            // Constraint::Length - is rather height. 3 - size of rem.(size of font)
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
-                .margin(2)
                 .constraints(
                     [
-                        Constraint::Length(3),
-                        Constraint::Min(2),
+                        Constraint::Length(2),
+                        Constraint::Min(1), // not shure what it means.
                         Constraint::Length(3),
                     ]
                     .as_ref(),
@@ -142,14 +144,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let tabs = Tabs::new(menu)
                 .select(active_menu_item.into())
-                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .block(Block::default())// .title("Menu").borders(Borders::BOTTOM))
                 .style(Style::default().fg(Color::White))
                 .highlight_style(Style::default().fg(Color::Yellow))
                 .divider(Span::raw("|"));
 
+            // render menu
             rect.render_widget(tabs, chunks[0]);
+
+            // render main area
             match active_menu_item {
-                MenuItem::Home => rect.render_widget(render_home(), chunks[1]),
+                MenuItem::Home => {
+                    rect.render_widget(render_home(), chunks[1]);
+                    // depending on insertion mode render different versions of widget.
+                    // rect.set_cursor(1,1);
+                },
                 MenuItem::Pets => {
                     let pets_chunks = Layout::default()
                         .direction(Direction::Horizontal)
@@ -165,22 +174,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             rect.render_widget(copyright, chunks[2]);
         })?;
 
-        match rx.recv()? {
-            Event::Input(event) => match event.code {
-                KeyCode::Char('q') => {
-                    disable_raw_mode()?;
+        if let Ok(event) = rx.recv() {
+            match event {
+                Event::Key(Key::Ctrl('q')) | Event::Key(Key::Ctrl('c')) => {
+                    // terminal.clear()?;
                     terminal.show_cursor()?;
                     break;
-                }
-                KeyCode::Char('h') => active_menu_item = MenuItem::Home,
-                KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
-                KeyCode::Char('a') => {
+                },
+                Event::Key(Key::Ctrl('h')) => active_menu_item = MenuItem::Home,
+                Event::Key(Key::Ctrl('p')) => active_menu_item = MenuItem::Pets,
+                Event::Key(Key::Ctrl('a')) => {
                     add_random_pet_to_db().expect("can add new random pet");
-                }
-                KeyCode::Char('d') => {
+                },
+                Event::Key(Key::Ctrl('d')) => {
                     remove_pet_at_index(&mut pet_list_state).expect("can remove pet");
-                }
-                KeyCode::Down => {
+                },
+                Event::Key(Key::Down) => {
                     if let Some(selected) = pet_list_state.selected() {
                         let amount_pets = read_db().expect("can fetch pet list").len();
                         if selected >= amount_pets - 1 {
@@ -189,8 +198,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             pet_list_state.select(Some(selected + 1));
                         }
                     }
-                }
-                KeyCode::Up => {
+                },
+                Event::Key(Key::Up) => {
                     if let Some(selected) = pet_list_state.selected() {
                         let amount_pets = read_db().expect("can fetch pet list").len();
                         if selected > 0 {
@@ -199,17 +208,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             pet_list_state.select(Some(amount_pets - 1));
                         }
                     }
-                }
-                _ => {}
-            },
-            Event::Tick => {}
+                },
+                _ => {},
+
+            }
+
         }
     }
-
     Ok(())
 }
 
-fn render_home<'a>() -> Paragraph<'a> {
+fn render_home<'a>() -> Paragraph<'a> 
+{
     let home = Paragraph::new(vec![
         Spans::from(vec![Span::raw("")]),
         Spans::from(vec![Span::raw("Welcome")]),
@@ -325,21 +335,21 @@ fn add_random_pet_to_db() -> Result<Vec<Pet>, Error> {
     let mut rng = rand::thread_rng();
     let db_content = fs::read_to_string(DB_PATH)?;
     let mut parsed: Vec<Pet> = serde_json::from_str(&db_content)?;
-    let catsdogs = match rng.gen_range(0, 1) {
-        0 => "cats",
+    let catsdogs = match rng.gen_range(0..=10) {
+        i if i % 2 == 0  => "cats",
         _ => "dogs",
     };
 
     let random_pet = Pet {
-        id: rng.gen_range(0, 9999999),
-        name: rng.sample_iter(Alphanumeric).take(10).collect(),
+        id: rng.gen_range(0..9999999),
         category: catsdogs.to_owned(),
-        age: rng.gen_range(1, 15),
+        age: rng.gen_range(1..15),
         created_at: Utc::now(),
+        name: rng.sample_iter(Alphanumeric).take(10).map(char::from).collect(),
     };
 
     parsed.push(random_pet);
-    fs::write(DB_PATH, &serde_json::to_vec(&parsed)?)?;
+    fs::write(DB_PATH, serde_json::to_vec(&parsed)?)?;
     Ok(parsed)
 }
 
@@ -348,8 +358,8 @@ fn remove_pet_at_index(pet_list_state: &mut ListState) -> Result<(), Error> {
         let db_content = fs::read_to_string(DB_PATH)?;
         let mut parsed: Vec<Pet> = serde_json::from_str(&db_content)?;
         parsed.remove(selected);
-        fs::write(DB_PATH, &serde_json::to_vec(&parsed)?)?;
-        let amount_pets = read_db().expect("can fetch pet list").len();
+        fs::write(DB_PATH, serde_json::to_vec(&parsed)?)?;
+        let _amount_pets = read_db().expect("can fetch pet list").len();
         if selected > 0 {
             pet_list_state.select(Some(selected - 1));
         } else {
